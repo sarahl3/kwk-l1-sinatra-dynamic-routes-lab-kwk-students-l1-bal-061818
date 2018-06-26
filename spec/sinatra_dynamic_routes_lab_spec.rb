13376ba05a0a39e3388d98de3e131830b2a22693
Sarah Li <sarcl@hotmail.com>
2018-06-26 19:54:46 +0000

    automatically backed up by learn

--- spec/sinatra_dynamic_routes_lab_spec.rb
+++ spec/sinatra_dynamic_routes_lab_spec.rb
@@ -88,8 +88,21 @@ describe App do
   describe 
   get /:operation/:number1/:number2' do
      @operation = params[operation]
-     @number1 = params[number1]
-     @number2 = params [number2]
+     @number1 = params[number1].to_i
+     @number2 = params [number2]to_i
+    if @operation == "add"
+       @number1 + @number2
+    elsif @operation == "subtract"
+        @number1 - @number2
+    elsif @operation == "multiply"
+        @number1 * @number2
+    else
+    @number1/@number2
+   
+   
+   
+   
+   
     it 'adds two numbers together' do
       get '/add/215/93' do
         215 = params[:215:].to_i
